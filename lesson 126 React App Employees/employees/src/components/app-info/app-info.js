dec54eb04237511f8ff191fbd2153a25203379ea
Andrey <luchko.andrey@gmail.com>
2023-01-28 22:28:01 +0200

    Work with immutable & rise state

--- lesson 126 React App Employees/employees/src/components/app-info/app-info.js
+++ lesson 126 React App Employees/employees/src/components/app-info/app-info.js
@@ -1,11 +1,12 @@
 import './app-info.css';
 
-const AppInfo = () => {
+const AppInfo = ({employeesTotal, employeesBonus}) => {
+
     return (
         <div className="app-info">
             <h1>Учет сотрудников в компании N</h1>
-            <h2>Общее число сотрудников: </h2>
-            <h2>Премию получат: </h2>
+            <h2>Общее число сотрудников: {employeesTotal} </h2>
+            <h2>Премию получат: {employeesBonus} </h2>
         </div>
     )
 }
--- lesson 126 React App Employees/employees/src/components/employees-add-form/employees-add-form.js
+++ lesson 126 React App Employees/employees/src/components/employees-add-form/employees-add-form.js
@@ -17,28 +17,44 @@ class EmployeesAddForm extends Component {
         })
     }
 
+    onSubmit  = (e) => {
+        e.preventDefault();
+
+        if(this.state.name.length > 3 && this.state.salary !== '') {
+           this.props.onAdd(this.state.name, this.state.salary);
+        } else {
+            return
+        }
+        this.setState({
+            name: '',
+            salary: ''
+        })
+    }
+
     render() {
-        const {name, salary} = this.state
+        const { name, salary } = this.state
 
         return (
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit={this.onSubmit}>
                     <input type="text"
                         className="form-control new-post-label"
                         placeholder="Как его зовут?"
                         name="name"
                         value={name}
-                        onChange={this.onValueChange}/>
+                        onChange={this.onValueChange} />
                     <input type="number"
                         className="form-control new-post-label"
                         placeholder="З/П в $?"
                         name="salary"
                         value={salary}
-                        onChange={this.onValueChange}/>
+                        onChange={this.onValueChange} />
 
-                    <button type="submit"
+                    <button
+                        type="submit"
                         className="btn btn-outline-light">Добавить</button>
                 </form>
             </div>
