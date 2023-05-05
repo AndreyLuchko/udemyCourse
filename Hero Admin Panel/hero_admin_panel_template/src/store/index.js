3da3fbaafe26a6af77030fc2d8e767e5dbbd74af
Andrey <luchko.andrey@gmail.com>
2023-05-05 18:33:02 +0300

    use applyMiddleware from redux

--- Hero Admin Panel/hero_admin_panel_template/src/store/index.js
+++ Hero Admin Panel/hero_admin_panel_template/src/store/index.js
@@ -1,29 +1,36 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const enhancer = (createStore) => (...arg) => {
-    const store = createStore(...arg);
-
-    const oldDispatch = store.dispatch;
-    store.dispatch = (action) => {
-        if (typeof action === 'string') {
-            return oldDispatch({
-                type: action
-            })
-        }
-        return oldDispatch(action);
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
     }
-    return store;
-}
+    return next(action);
+};
+
+// const enhancer = (createStore) => (...arg) => {
+//     const store = createStore(...arg);
+
+//     const oldDispatch = store.dispatch;
+//     store.dispatch = (action) => {
+//         if (typeof action === 'string') {
+//             return oldDispatch({
+//                 type: action
+//             })
+//         }
+//         return oldDispatch(action);
+//     }
+//     return store;
+// }
 
 const store = createStore( 
     combineReducers({heroes, filters}),
-    compose(
-        enhancer,
+    compose (applyMiddleware(stringMiddleware),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     ));
-
 
 export default store;
 
